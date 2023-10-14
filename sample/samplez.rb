fdff882863aa7f593b27a34e222033db9a2570fa
siranovel <siranovel@gmail.com>
2023-10-14 11:05:48 +0900

    Add files via upload

--- sample/samplez.rb
+++ sample/samplez.rb
@@ -16,9 +16,6 @@ RSpec.describe Num4HypothTestLib do
         it '#fDistTest' do
             expect(hypothTest.fDistTest(0.4727, 11, 7, a)).to eq false
         end
-        it '#gnTest' do
-            expect(hypothTest.gnTest(2.3724, 8, a)).to eq true
-        end
     end
     describe Num4HypothTestLib::RightSideTestLib do
         let!(:hypothTest) { Num4HypothTestLib::RightSideTestLib.new }
@@ -39,9 +36,6 @@ RSpec.describe Num4HypothTestLib do
         it '#fDistTest' do
             expect(hypothTest.fDistTest(0.4727, 11, 7, a)).to eq false
         end
-        it '#gnTest' do
-            expect(hypothTest.gnTest(2.3724, 8, a)).to eq true
-        end
     end
     describe Num4HypothTestLib::LeftSideTestLib do
         let!(:hypothTest) { Num4HypothTestLib::LeftSideTestLib.new }
@@ -56,6 +50,15 @@ RSpec.describe Num4HypothTestLib do
         end
         it '#fDistTest' do
             expect(hypothTest.fDistTest(0.4727, 11, 7, a)).to eq false
+        end
+    end
+    describe Num4HypothTestLib::GrubbsTestLib do
+        let!(:hypothTest) { Num4HypothTestLib::GrubbsTestLib.new }
+        it '#twoSideTest' do
+            expect(hypothTest.twoSideTest(2.3724, 8, a)).to eq true
+        end
+        it '#oneSideTest' do
+            expect(hypothTest.oneSideTest(2.3724, 8, a)).to eq true
         end
     end
 end
