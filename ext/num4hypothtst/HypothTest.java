35bbe120a9ad3fb41ab630ebb427cff58f8fe2af
siranovel <siranovel@gmail.com>
2023-10-14 11:02:41 +0900

    Delete ext/num4hypothtst/HypothTest.java
    delete file of HypothTest.java.

--- ext/num4hypothtst/HypothTest.java
+++
@@ -1,9 +0,0 @@
-// 仮設検定
-interface HypothTest {
-    boolean tDistTest(double statistic, double df, double a);
-    boolean chi2DistTest(double statistic, double df, double a);
-    boolean normDistTest(double statistic, double a);
-    boolean fDistTest(double statistic, double nf, double df, double a);
-    boolean gnTest(double statistic, int n, double a);
-}
-
