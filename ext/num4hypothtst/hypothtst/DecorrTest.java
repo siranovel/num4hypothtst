83208f2e15446c73f2302e3ef5b748f02e3315a6
siranovel <24727362+siranovel@users.noreply.github.com>
2025-11-10 11:05:54 +0900

    Add files via upload

--- ext/num4hypothtst/hypothtst/DecorrTest.java
+++ ext/num4hypothtst/hypothtst/DecorrTest.java
@@ -1,16 +1,8 @@
 package hypothtst;
 
-import org.apache.commons.math3.distribution.TDistribution;
 public class DecorrTest implements HypothTest {
     private static HypothTest hypothTest = new DecorrTest();
     public static HypothTest getInstance() {
         return hypothTest;
-    }
-    public boolean twoSideTest(double r, int n, double a) {
-        double statistic = Math.abs(r) * Math.sqrt(n - 2.0) / Math.sqrt(1.0 - r * r);
-        TDistribution tDist = new TDistribution(n - 2);
-        double p = 1.0 - tDist.cumulativeProbability(statistic);
-
-        return (p < a) ? true : false;
     }
 }
--- ext/num4hypothtst/hypothtst/HypothTest3.java
+++ ext/num4hypothtst/hypothtst/HypothTest3.java
@@ -7,6 +7,7 @@ interface HypothTest3 {
     boolean normDistTest(double statistic, double a);
     boolean fDistTest(double statistic, double nf, double df, double a);
 
+    // 母相関係数の検定
     default boolean populationCorre(double r, int n, double rth0, double a) {
         return normDistTest(populaCorrStatistic(r, n, rth0), a);
     }
@@ -17,6 +18,15 @@ interface HypothTest3 {
              - 0.5 * Math.log((1.0 + rth0) / (1.0 - rth0))
                );
 
+        return statistic;
+    }
+    // 母相関係数の差の検定
+    default boolean diffPopulationCorre(double r1, int n1, double r2, int n2, double a) {
+        return normDistTest(diffPopulationCorreStatistic(r1, n1, r2, n2), a);
+    }
+    private double diffPopulationCorreStatistic(double r1, int n1, double r2, int n2) {
+        double statistic = (0.5 * Math.log((1 + r1) / (1-r1)) - 0.5 * Math.log((1 + r2) / (1-r2)))
+        / Math.sqrt((1.0 / (n1 - 3.0)) + (1.0 / (n2 - 3)));
         return statistic;
     }
 }
